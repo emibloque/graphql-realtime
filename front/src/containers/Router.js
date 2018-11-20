a7a4064f684a61182f084748d2007960020bbd64
Luis Emilio Velasco Sanchez <emibloque@gmail.com>
2018-11-20 01:02:05 +0100

    Add simple react front

---
+++ front/src/containers/Router.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import MainNav from "../components/MainNav";
+import Home from "../containers/Home";
+import Live from "../containers/Live";
+
+const Router = () => (
+  <BrowserRouter>
+    <div>
+      <MainNav />
+      <main>
+        <Route path="/" exact component={Home} />
+        <Route path="/live/" component={Live} />
+      </main>
+    </div>
+  </BrowserRouter>
+);
+
+export default Router;
---
+++ front/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Router from './containers/Router';
+
+ReactDOM.render(<Router />, document.getElementById('root'));
