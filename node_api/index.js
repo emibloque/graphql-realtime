ee1db1b06dab94b56fb875b57aec135d1dddc311
Luis Emilio Velasco Sanchez <emibloque@gmail.com>
2018-11-23 00:04:40 +0100

    Add posts and users

--- node_api/index.js
+++ node_api/index.js
@@ -1,27 +1,57 @@
 const express = require("express");
 const { buildSchema } = require("graphql");
 const graphqlHTTP = require("express-graphql");
+const postsList = require('./posts');
+
+const port = 3000;
 
 const schema = buildSchema(`
-  type Story {
-    image: String!
+  type User {
+    id: ID!
+    name: String!
   }
 
+  type Post {
+    id: ID!
+    image: String!
+    likes: Int!
+    author: User!
+    comments: [Comment]
+  }
+
+  type Comment {
+    id: ID!
+    text: String!
+    author: User!
+  }
+
+
+
+
   type Query {
-    stories: [Story]
+    posts: [Post]
+  }
+
+  type Mutation {
+    addLike(postId: ID): Post
   }
 `);
 
 const root = {
-  stories: () => {
-    return [
-      { image: "url1" },
-      { image: "url2" },
-      { image: "url33" },
-      { image: "url1" },
-      { image: "url2" },
-      { image: "url33" }
-    ];
+  posts: () => {
+    return postsList;
+  },
+
+  addLike: ({ postId }) => {
+    const id = parseInt(postId);
+    for (const post of postsList) {
+      if (post.id === id) {
+        post.likes++;
+        return post;
+      }
+    }
+
+    throw Error('Post not found');
   }
 };
 
@@ -35,5 +65,5 @@ app.use(
   })
 );
 
-app.listen(3000);
+app.listen(port);
 console.log("GraphQL API server running at localhost:" + port);
---
+++ node_api/posts.js
@@ -0,0 +1,70 @@
+module.exports = [
+  {
+    id: 1,
+    author: { id: 1, name: "charlotte" },
+    likes: 28,
+    image: "/images/cassidy-mills-425636-unsplash.jpg",
+    comments: [
+      {
+        id: 1,
+        author: { id: 1, name: "charlotte" },
+        text: "Hey there!"
+      },
+      {
+        id: 2,
+        author: { id: 2, name: "laosemat" },
+        text: "Sunrises are always beautiful."
+      },
+      {
+        id: 3,
+        author: { id: 3, name: "gustavo" },
+        text: "Nice shot 🔥"
+      }
+    ]
+  },
+  {
+    id: 2,
+    author: { id: 4, name: "kevin" },
+    likes: 43,
+    image: "/images/eamonn-maguire-57073-unsplash.jpg",
+    liked: true,
+    comments: [
+      {
+        id: 4,
+        author: { id: 4, name: "kevin" },
+        text: "With the family."
+      },
+      {
+        id: 5,
+        author: { id: 1, name: "charlotte" },
+        text: "Omg"
+      }
+    ]
+  },
+  {
+    id: 3,
+    author: { id: 5, name: "doris" },
+    likes: 64,
+    image: "/images/ian-parker-546258-unsplash.jpg",
+    comments: [
+      {
+        id: 6,
+        author: { id: 5, name: "doris" },
+        text: "Alone in the wild"
+      }
+    ]
+  },
+  {
+    id: 4,
+    author: { id: 3, name: "gustavo" },
+    likes: 49,
+    image: "/images/maciej-ostrowski-1133088-unsplash.jpg",
+    comments: [
+      {
+        id: 7,
+        author: { id: 3, name: "gustavo" },
+        text: "Diving..."
+      }
+    ]
+  }
+];
